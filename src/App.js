9f41cbf1779fcc854f690a7c2189c29f240c3712
andrparsons <andrparsons@gmail.com>
2018-03-09 17:22:39 -0500

    Proper dither conversion

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
 
-import Camera from './components/camera'
+import Camera from "./components/camera";
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <div className="photobooth">
-          
-          <div className="controls">
-            <p>Controls here</p>
-          </div>
-          <Camera />
-        </div>
-      </div>
-    );
+    return <Camera />;
   }
 }
 
